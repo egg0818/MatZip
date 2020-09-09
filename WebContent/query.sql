09444f2c0fa590684cd1eee8db867bf109b44ae0
taddy <taddy0818@naver.com>
2020-09-09 17:29:52 +0900

    세팅 및 로그인 구현 중

---
+++ WebContent/query.sql
@@ -0,0 +1,12 @@
+CREATE TABLE t_user(
+	i_user INT unsigned PRIMARY KEY AUTO_INCREMENT,
+	user_id varchar(30) NOT NULL UNIQUE,
+	user_pw varchar(70) NOT NULL,
+	salt VARCHAR(30) NOT NULL,
+	nm       varchar(5) NOT NULL,
+	profile_img VARCHAR(50),
+	r_dt DATETIME DEFAULT NOW(),
+	m_dt DATETIME DEFAULT NOW()
+);
+
+SELECT * FROM t_user;
