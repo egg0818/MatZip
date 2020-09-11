c8c137e41504415249af9573338cac6d7ae2a636
taddy <taddy0818@naver.com>
2020-09-11 12:30:22 +0900

    가게등록 구현 중(2)

--- src/com/koreait/matzip/restaurant/RestaurantController.java
+++ src/com/koreait/matzip/restaurant/RestaurantController.java
@@ -2,6 +2,7 @@ package com.koreait.matzip.restaurant;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.koreait.matzip.CommonDAO;
 import com.koreait.matzip.Const;
 import com.koreait.matzip.ViewRef;
 
@@ -14,6 +15,9 @@ public class RestaurantController {
 	   }
 	
 	public String restReg(HttpServletRequest request) {
+		  final int I_M = 1; // 카테고리 코드
+		  request.setAttribute("categoryList", CommonDAO.selCodeList(I_M));
+		
 	      request.setAttribute(Const.TITLE, "가게등록");
 	      request.setAttribute(Const.VIEW, "restaurant/restReg");
 	      return ViewRef.TEMP_MENU_TEMP;
